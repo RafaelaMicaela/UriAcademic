591fbee7d9f77fabef8a9d8dd3b0d2ad3018da0d
rafaela <rafaela.micaela.77@gmail.com>
2018-04-08 00:38:00 -0300

    first commit

--- Lista05/2402.py
+++ Lista05/2402.py
@@ -1,7 +1,15 @@
-n = int(input())
-i = int()
-for x in range(0,10):
-	if (n % x == 0):
-		print("S")
-	else:
-		print("N")
+num = int(input())
+c = 0
+for x in range(2,int(num//2+1)):
+  if (num%x==0):
+    c+=1
+    print('S')
+    break
+if(c==0):
+  print('N')
+		
+		
+
+
+
+
